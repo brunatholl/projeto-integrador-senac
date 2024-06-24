18279e9e412082bcdc53a0c40bf458b739fe6f26
BrunaTholl <94844452+brunatholl@users.noreply.github.com>
2024-06-24 18:24:10 -0300

    ok

---
+++ apiareaazul-main/src/main/java/bancocrudspringboot/repository/CartaoRepository.java
@@ -0,0 +1,11 @@
+package bancocrudspringboot.repository;
+
+import bancocrudspringboot.model.Cartao;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CartaoRepository extends JpaRepository<Cartao, Long>{
+    
+    
+}
---
+++ apiareaazul-main/src/main/java/bancocrudspringboot/repository/VeiculoRepository.java
@@ -0,0 +1,12 @@
+package bancocrudspringboot.repository;
+
+import bancocrudspringboot.model.Veiculo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface VeiculoRepository extends JpaRepository<Veiculo, Long>{
+    
+    
+}
